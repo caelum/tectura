24a8145965fe05b5b187984606b9e91677faf4eb
Pedro Matiello <pmatiello@gmail.com>
2011-04-19 13:40:44 -0300

    Still trying to make Hudson work.

--- hudson-build.sh
+++ hudson-build.sh
@@ -54,7 +54,7 @@ cp config/database.yml.example config/database.yml
 cp config/email.yml.example config/email.yml
 cp config/recaptcha.rb.example config/recaptcha.rb
 
-rake db:create:test
+rake db:create
 rake db:migrate
 
 rake spec
